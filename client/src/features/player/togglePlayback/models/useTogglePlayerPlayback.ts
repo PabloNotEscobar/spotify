cc048d9434c4866d0d3a3c226687fc434286b28a
PabloNotEscobar <0kvestik0@gmail.com>
2026-03-19 16:44:27 +0300

    title changed, play fixed

--- client/src/features/player/togglePlayback/models/useTogglePlayerPlayback.ts
+++ client/src/features/player/togglePlayback/models/useTogglePlayerPlayback.ts
@@ -1,6 +1,7 @@
 'use client'
 import { usePlayerStore } from "@/widgets/player/model/player-store";
 import { throttle } from 'es-toolkit';
+import {useAudio} from "@/shared/providers/audio-provider";
 
 const throttledAction = throttle((action: () => void) => {
     action();
@@ -10,12 +11,16 @@ export const useTogglePlayerPlayback = () => {
     const play = usePlayerStore(state => state.play);
     const setPlay = usePlayerStore(state => state.setPlay);
     const setPause = usePlayerStore(state => state.setPause);
+    const {audioRef} = useAudio()
+    const audio = audioRef.current
 
     const playHandler = () => {
         throttledAction(() => {
             if (!play) {
+                audio.play()
                 setPlay();
             } else {
+                audio.pause()
                 setPause();
             }
         });
